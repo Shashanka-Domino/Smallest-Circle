db0e9732fd9c8f013f25f66282f2590274f5f143
shashanka1 <shashankasrirama@gmail.com>
2016-04-28 13:49:52 -0500

    Added files via upload

---
+++ buster.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+var config = module.exports;
+
+config["My tests"] = {
+    rootPath: "../",
+    environment: "browser", // or "node"
+    sources: [
+        "lib/mylib.js",
+        "lib/**/*.js"
+    ],
+    tests: [
+        "test/*-test.js"
+    ]
+}
+
+// Add more configuration groups as needed
